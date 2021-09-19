32cd1df22706098b712fb10e3c92791296f63634
ProgWon <dhwhc0711@naver.com>
2021-09-19 18:33:08 +0900

    feat: "Write Type of Variable"
    Write Type of Variable

--- app.ts
+++ app.ts
@@ -1,8 +1,23 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+}
+const container : HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: []
 };
@@ -14,6 +29,11 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
+function updateView(html) {
+  if (container !== null) container.innerHTML = html;
+  else console.error('No Root Element!!')
+}
+
 function makeFeeds(feeds) {
   for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
@@ -23,7 +43,7 @@ function makeFeeds(feeds) {
 }
 
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -87,7 +107,7 @@ function newsFeed() {
   );
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
 
-  container.innerHTML = template;
+  updateView(template)
 }
 
 function newsDetail() {
@@ -151,7 +171,7 @@ function newsDetail() {
     return commentString.join('');
   }
 
-  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+  updateView(template)
 }
 
 function router() {
