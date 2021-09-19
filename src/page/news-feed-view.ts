ee3e59dcf119bf4d0f047cb0646b16829ecb74f0
ProgWon <dhwhc0711@naver.com>
2021-09-20 02:34:31 +0900

    feat: "Change to asynchronous function"
    Change to asynchronous function

--- src/page/news-feed-view.ts
+++ src/page/news-feed-view.ts
@@ -1,7 +1,6 @@
 import View from '../core/view';
 import { NewsFeedApi } from '../core/api';
-import { NewsFeed } from '../types';
-import { NEWS_URL } from '../config';
+import { NewsStore } from '../types';
 
 const template = `
 <div class="bg-gray-600 min-h-screen">
@@ -29,28 +28,27 @@ const template = `
 `;
 
 export default class NewsFeedView extends View {
-    private api: NewsFeedApi;
-    private feeds: NewsFeed[];
+  private api: NewsFeedApi;
+  private store: NewsStore;
 
-    constructor(containerId: string) {
-        super(containerId, template);
+  constructor(containerId: string, store: NewsStore) {
+    super(containerId, template);
 
-        this.feeds = window.store.feeds;
-        this.api = new NewsFeedApi(NEWS_URL);
+    this.store = store;
+    this.api = new NewsFeedApi();
+  }
 
-        if (window.store.feeds.length === 0) {
-            this.feeds = window.store.feeds = this.api.getData();
-            this.makeFeeds();
-        }
+  render = async (page: string = '1'): Promise<void> => {
+    this.store.currentPage = Number(page);
+
+    if (!this.store.hasFeeds) {
+      this.store.setFeeds(await this.api.getData());
     }
 
-    render = (page: string = '1'): void => {
-        window.store.currentPage = Number(page);
+    for (let i = (this.store.currentPage - 1) * 10; i < this.store.currentPage * 10; i++) {
+      const { id, title, comments_count, user, points, time_ago, read } = this.store.getFeed(i);
 
-        for (let i = (window.store.currentPage - 1) * 10; i < window.store.currentPage * 10; i++) {
-            const { id, title, comments_count, user, points, time_ago, read } = this.feeds[i];
-
-            this.addHtml(`
+      this.addHtml(`
         <div class="p-6 ${read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
           <div class="flex">
             <div class="flex-auto">
@@ -69,18 +67,12 @@ export default class NewsFeedView extends View {
           </div>
         </div>    
       `);
-        }
-
-        this.setTemplateData('news_feed', this.getHtml());
-        this.setTemplateData('prev_page', String(window.store.currentPage > 1 ? window.store.currentPage - 1 : 1));
-        this.setTemplateData('next_page', String(window.store.currentPage + 1));
-
-        this.updateView();
     }
 
-    private makeFeeds(): void {
-        for (let i = 0; i < this.feeds.length; i++) {
-            this.feeds[i].read = false;
-        }
-    }
+    this.setTemplateData('news_feed', this.getHtml());
+    this.setTemplateData('prev_page', String(this.store.prevPage));
+    this.setTemplateData('next_page', String(this.store.nextPage));
+
+    this.updateView();
+  }
 }
